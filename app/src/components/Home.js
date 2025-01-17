b9a940ee9b2be2be0a2500a7243176cfffcec82c
Lou-Anne Gautherie <lougautr@gmail.com>
2025-01-17 09:22:39 +0100

    transfer to MAC

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../css/Home.css';
 
 const Home = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="home-content">
-            Home
-        </div>
+        <>
+            <header className="sticky-header">
+                <nav>
+                    <ul>
+                        <li onClick={() => navigate('/')}>Accueil</li>
+                        <li onClick={() => navigate('/about')}>À propos</li>
+                        <li onClick={() => navigate('/contact')}>Contact</li>
+                    </ul>
+                </nav>
+            </header>
+            <div className="home-background">
+                <div className="home-content">
+                    <h1>Bienvenue à Caen</h1>
+                    <p>Découvrez notre contenu par-dessus une belle carte !</p>
+                </div>
+            </div>
+        </>
     );
 };
 
