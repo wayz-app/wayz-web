e25354fa7f606efa4b619e286eeaeb10779838a8
Enzo DJABALI <enzo.djabali@supinfo.com>
2025-02-26 22:55:28 +0100

    feat(#10): setting up express and a host endpoint to display in footer

---
+++ app/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const os = require('os');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/host', (req, res) => {
+    res.json({ hostname: os.hostname() });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
